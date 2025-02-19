22d642d44984e0cb65b63a8a0ea5b2282d77a012
向海 <web_haixiang@sina.com>
2025-02-19 17:42:01 +0800

    feat: commit init

--- storybook-static/assets/tokens-CkATKTW6.js
+++
@@ -1,1 +0,0 @@
-const r={colorBrandForeground2:"var(--colorBrandForeground2)",colorBrandBackground2:"var(--colorBrandBackground2)",colorPaletteBerryBackground1:"var(--colorPaletteBerryBackground1)"};export{r as t};
